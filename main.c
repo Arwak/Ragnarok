b0223635e8657ffa5c7095abb286797bb3467c0f
clupspv <claudia.peiro.2015@salleurl.edu>
2018-04-11 17:45:57 +0200

    #initialization

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+#define N_ARGS      3
+
+#define MSG_ERR_ARGS        "Invalid number of arguments.\n"
+#define MSG_ERR_FITXER      "Error amb el fitxer\n"
+
+
+int main(int argc, char * argv[]) {
+
+    if (argc != N_ARGS) {
+        printf(MSG_ERR_ARGS);
+        return EXIT_FAILURE;
+    }
 }
